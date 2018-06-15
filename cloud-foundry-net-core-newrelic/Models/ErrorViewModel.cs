cf5d712cc08147be3652aae098f66622bee686ac
Cedric Yao <raspar@gmail.com>
2018-06-15 08:49:34 -0500

    added gitignore

---
+++ cloud-foundry-net-core-newrelic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cloud_foundry_net_core_newrelic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
